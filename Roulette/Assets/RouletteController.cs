96ca0e9db63a7392addf0539bd70a9ea6924e23d
Jinwoo, Kim <admin@leaprealm.com>
2020-09-06 22:30:33 +0900

    Roulette Game

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    private float rotSpeed = 0; // 회전 속도
+
+    private void Update()
+    {
+        // 클릭하면 회전 속도를 설정한다.
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 30;
+        }
+
+        // 회전 속도만큼 룰렛을 회전시킨다.
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        // 룰렛을 감속시킨다.
+        this.rotSpeed *= 0.99f;
+    }
+}
