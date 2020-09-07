09ca9de3a163b0e14ec7975c9eee6ee44dc3ac85
Jinwoo, Kim <admin@leaprealm.com>
2020-09-07 12:06:50 +0900

    Swipe Car Game

---
+++ 2_SwipeCar/Assets/Scripts/CarController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float speed = 0;
+    private Vector2 startPos;
+
+    private void Update()
+    {
+        // 스와이프 길이를 구한다.
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 마우스를 클릭한 좌표
+            this.startPos = Input.mousePosition;
+        } 
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // 마우스 버튼에서 손가락을 떼었을 때 좌표
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            // 스와이프 길이를 처음 속도로 변경한다.
+            this.speed = swipeLength / (Screen.width / 100.0f);
+
+            // 효과음 재생
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+        this.speed *= 0.99f;
+    }
+}
---
+++ 2_SwipeCar/Assets/Scripts/GameDirector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // UI를 사용하기 위해 필요
+
+public class GameDirector : MonoBehaviour
+{
+    private GameObject car;
+    private GameObject flag;
+    private GameObject distance;
+
+    private void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    private void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+
+        if (length >= 0)
+        {
+            this.distance.GetComponent<Text>().text = "목표 지점까지 " + length.ToString("F2") + "m";
+        }
+        else
+        {
+            this.distance.GetComponent<Text>().text = "게임 오버";
+        }
+    }
+}
